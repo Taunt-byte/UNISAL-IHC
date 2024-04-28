f6e74ecb86aa7814c9ad26579154df394e5e3499
Lucas Neves <71947586+Taunt-byte@users.noreply.github.com>
2024-04-28 00:38:41 -0300

    Update Index.html

--- assets/main.js
+++ assets/main.js
@@ -1,37 +1,106 @@
 // Array de produtos com descrição e preço
 var produtos = [
-  { descricao: "Livro 1", preco: 20.5 },
-  { descricao: "Livro 2", preco: 15.75 },
-  { descricao: "Livro 3", preco: 20.5 },
-  { descricao: "Livro 4", preco: 15.5 },
-  { descricao: "Livro 5", preco: 20.0 },
-  { descricao: "Livro 6", preco: 20.75 },
-  { descricao: "Revista 1", preco: 5.0 },
-  { descricao: "Revista 2", preco: 5.0 },
-  { descricao: "Revista 3", preco: 5.0 },
+  {
+    descricao: "Harry Potter e a Pedra Filosofal",
+    preco: 39.9,
+    imagem: "https://m.media-amazon.com/images/I/51lRMYwP-4L.jpg",
+  },
+  {
+    descricao: "O Senhor dos Anéis: A Sociedade do Anel",
+    preco: 49.9,
+    imagem:
+      "https://m.media-amazon.com/images/I/81MZ8OjmQrL._AC_UF1000,1000_QL80_.jpg",
+  },
+  {
+    descricao: "Cem Anos de Solidão",
+    preco: 29.9,
+    imagem:
+      "https://m.media-amazon.com/images/I/817esPahlrL._AC_UF1000,1000_QL80_.jpg",
+  },
+  {
+    descricao: "Dom Quixote",
+    preco: 34.9,
+    imagem:
+      "https://m.media-amazon.com/images/I/71LzWaIbBoL._AC_UF1000,1000_QL80_.jpg",
+  },
+  {
+    descricao: "As Crônicas de Nárnia: O Leão, a Feiticeira e o Guarda-Roupa",
+    preco: 24.9,
+    imagem:
+      "https://m.media-amazon.com/images/I/7158aW38zxL._AC_UF1000,1000_QL80_.jpg",
+  },
+  {
+    descricao: "O Pequeno Príncipe",
+    preco: 19.9,
+    imagem:
+      "https://m.media-amazon.com/images/I/71LJ4k-k9hL._AC_UF1000,1000_QL80_.jpg",
+  },
+  {
+    descricao: "Revista Veja - Edição Especial",
+    preco: 9.9,
+    imagem:
+      "https://veja.abril.com.br/wp-content/uploads/2018/09/capa-25611.jpg?quality=90&strip=info&w=570&h=750&crop=1",
+  },
+  {
+    descricao: "Revista National Geographic - Edição de Janeiro",
+    preco: 12.9,
+    imagem:
+      "https://m.media-amazon.com/images/I/71BBlSghI2L._AC_UF894,1000_QL80_.jpg",
+  },
+  {
+    descricao: "Revista Super Interessante - Edição Limitada",
+    preco: 7.9,
+    imagem:
+      "https://super.abril.com.br/wp-content/uploads/2023/02/SI_448_CAPA.png?w=1024",
+  },
 ];
 
+var promocao = [
+  {
+    descricao: "Harry Potter e a Pedra Filosofal",
+    preco: 39.9,
+    new: 23.5,
+    imagem: "https://m.media-amazon.com/images/I/51lRMYwP-4L.jpg",
+  },
+  {
+    descricao: "O Pequeno Príncipe",
+    preco: 19.9,
+    new: 14.5,
+    imagem:
+      "https://m.media-amazon.com/images/I/71LJ4k-k9hL._AC_UF1000,1000_QL80_.jpg",
+  },
+];
 // Função para gerar os produtos no HTML
 function gerarProdutos() {
   var container = document.getElementById("produtos");
-
   produtos.forEach(function (produto) {
     var produtoHTML = `
-                    <div class="produto">
-                        <h2>${produto.descricao}</h2>
-                        <p>Preço: R$ ${produto.preco.toFixed(2)}</p>
-                    </div>
-                `;
+          <div class="produto">
+              <h2>${produto.descricao}</h2>
+              <img class="imagem-produto" src="${produto.imagem}">
+              <p>Preço: R$ ${produto.preco.toFixed(2)}</p>
+          </div>
+      `;
     container.innerHTML += produtoHTML;
   });
 }
-
-// Chama a função para gerar os produtos ao carregar a página
-window.onload = gerarProdutos;
-// Função para alternar entre os modos claro e escuro
-function alternarModoEscuro() {
-    document.body.classList.toggle('modo-escuro');
+function gerarPromo() {
+  var container = document.getElementById("promocao");
+  promocao.forEach(function (promocao) {
+    var promocaoHTML = `
+    <div class="promocao">
+    <h2>${promocao.descricao}</h2>
+    <img class="imagem-produto" src="${promocao.imagem}">
+    <p>Preço: R$ ${promocao.preco.toFixed(2)}</p>
+    <p>Novo Preço: R$ ${promocao.new.toFixed(2)}</p>
+</div>
+    
+    `;
+    container.innerHTML += promocaoHTML;
+  });
 }
-
-// Adiciona um evento de clique ao botão para chamar a função
-document.getElementById('modo-escuro').addEventListener('click', alternarModoEscuro);
+// Chama a função para gerar os produtos ao carregar a página
+window.onload = function() {
+  gerarProdutos();
+  gerarPromo();
+};
